d89623b3ffb82ba85253935ba61c61fe29a6e419
Bailey de Villiers <bailey.devilliers@gmail.com>
2021-09-13 18:32:08 +0400

    Integrated SQLAlchemy and PSQL

--- webapp/app.py
+++ webapp/app.py
@@ -3,18 +3,38 @@ from flask import Flask, render_template
 # import all forms
 from forms import *
 
-# App configuration
+# databse
+from models import *
+
+# Configure app
 app = Flask(__name__)
 app.secret_key = "password"
+
+# Configure db
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://vevrmxjgsdtlac:751dd9301bc9599bbbd483b5a9dc72d3dc027fce70d7d37382b29625c4cf43b6@ec2-54-220-14-54.eu-west-1.compute.amazonaws.com:5432/dbbn7lbgl3cn00"
+db = SQLAlchemy(app)
 
 @app.route("/", methods = ["GET","POST"])
 def index():
     
-    registration_form = RegistrationForm()
-    if registration_form.validate_on_submit():
-        return "Horray!"
+    reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        username = reg_form.username.data
+        password = reg_form.password.data
+        
+        # Check username uniqueness
+        user_obj = User.query.filter_by(username = username).first()
+        if user_obj:
+            return "Username taken"
+        
+        # add to DB
+        user = User(username = username,
+                    password = password)
+        db.session.add(user)
+        db.session.commit()
     
-    return render_template("index.html", form = registration_form)
+    return render_template("index.html", form = reg_form)
 
 if __name__ == "__main__":
     app.run(debug = True)
---
+++ webapp/create.py
@@ -0,0 +1,2 @@
+
+# instantiates database
